e6c9216de0481aba0c976d63ec76e20b2a9b5411
Vishakharajput00 <rajputvr00feb@gmail.com>
2023-07-15 10:56:43 +0530

    devide in components

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,46 +4,30 @@ import { MdAssessment } from "react-icons/md";
 import { MdOutlineLibraryBooks } from "react-icons/md";
 import { GrStatusUnknown } from "react-icons/gr";
 import "./Home.css";
+import SideNav from "./SideNav";
+import UpperHeader from "./UpperHeader";
+import AssessOverview from "./AssessOverview";
+import MyAssess from "./MyAssess";
 
 export default function Home() {
   return (
     <div>
       <div class="row">
-        <div class="column">
-          <div class="box">
-            <BiSolidDashboard style={{ height: "20px", width: "20px" }} />
-            <h4 class="heading">Dashboard</h4>
-          </div>
-          <div class="box1">
-            <MdAssessment style={{ height: "20px", width: "20px" }} />
-            <h4 class="heading">Assessment</h4>
-          </div>
-          <div class="box">
-            <MdOutlineLibraryBooks style={{ height: "20px", width: "20px" }} />
-            <h4 class="heading">My Library</h4>
-          </div>
-          <hr class="hr" />
-
-          <div class="box">
-            <div>
-              {" "}
-              <button class="btn">Admin</button>
-            </div>
-            <GrStatusUnknown
-              style={{ height: "20px", width: "20px", paddingTop: "15px" }}
-            />
-            <h4 class="heading">Round Status</h4>
-          </div>
-        </div>
+     <div class='column'>
+      <SideNav/>
+     </div>
 
         <div>
-          <div class="assess">
+          {/* <div class="assess">
             <div class='upperAssess'>
 <span class="firstHead">Assessment</span>
 <span>My Assessments</span>
             </div>
             <div>hello</div>
-          </div>
+          </div> */}
+          <UpperHeader/>
+          <AssessOverview/>
+          <MyAssess/>
         </div>
       </div>
     </div>
---
+++ src/components/UpperHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./UpperHeader.css";
+
+export default function UpperHeader() {
+  return (
+ <div class='assess'>
+    <span class="firstHead">Assessment</span>
+    <span>My Assessments</span>
+                </div>
+  )
+}
