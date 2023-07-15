4b33c3df1d5ff78b3fa9bac71a0bc8b17cd3cbb5
Vishakharajput00 <rajputvr00feb@gmail.com>
2023-07-15 21:06:03 +0530

    overview

--- src/components/MyAssess.js
+++ src/components/MyAssess.js
@@ -1,26 +1,115 @@
-import React from 'react';
+import React from "react";
 import { AiOutlinePlus } from "react-icons/ai";
-import  "./MyAssess.css"
+import "./MyAssess.css";
+import { PiBagFill } from "react-icons/pi";
+import { SlCalender } from "react-icons/sl";
+import { CiMenuKebab } from "react-icons/ci";
+import { AiOutlineLink } from "react-icons/ai";
+
 export default function MyAssess() {
+    const BoxObject=[
+        {
+            heading:"Math Assessment",
+            content:"Job",
+            duration:"00",
+            question:"00"
+        },
+        {
+            heading:"Math Assessment",
+            content:"Job",
+            duration:"00",
+            question:"00"
+        },  {
+            heading:"Math Assessment",
+            content:"Job",
+            duration:"00",
+            question:"00"
+        },
+        {
+            heading:"Math Assessment",
+            content:"Job",
+            duration:"00",
+            question:"00"
+        },
+        {
+            heading:"Math Assessment",
+            content:"Job",
+            duration:"00",
+            question:"00"
+        }
+    ]
   return (
     <div>
-      {/* heading  */}
-      <div> <h2>My Assessment</h2></div>
+    
+      <div>
+    
+        <h2>My Assessment</h2>
+      </div>
 
-      {/* 3 cards structure*/}
       <div class="cardsMainBox">
-        {/* 1st card */}
         <div class="newAssissmentBox">
-          <div><AiOutlinePlus></AiOutlinePlus></div>
+          <div class="Plusicon">
+            <AiOutlinePlus
+              style={{ color: "#0073E6", height: "23.33px", width: "23.33px" }}
+            />
+          </div>
           <span>New Assessment</span>
-          <p>From here you can add questions of multiple types like MCQs, subjective (text or paragraph)!</p>
+          <p>
+            From here you can add questions of multiple types like MCQs,
+            subjective (text or paragraph)!
+          </p>
         </div>
-        {/* 2nd card */}
-        <div  class="cards2Box">
-          <div></div>
-          <div></div>
+        {BoxObject && BoxObject.map((obj)=>{
+return(
+    <div class="cards2Box">
+          <div class="secondCard">
+            <div>
+              <div class="secondBoxIcon">
+                <PiBagFill class="secondicon" />
+              </div>
+              <div class="math">{obj.heading}</div>
+              <div>
+                <span style={{ color: "#1C4980", fontWeight: "500" }}>
+                  {obj.content}
+                </span>{" "}
+                |{" "}
+                <span style={{ color: "#8DA4BF" }}>
+                  <SlCalender class="calicon" /> 20 apr 2023
+                </span>
+              </div>
+            </div>
+            <div>
+              <CiMenuKebab />
+            </div>
+          </div>
+       
+          <div class="duration">
+            <div style={{ display: "flex" }}>
+              <span class="question">
+                {obj.duration} <br />
+                Duration{" "}
+              </span>
+              <span class="question" style={{ marginLeft: "10px" }}>
+                {obj.question} <br /> Question
+              </span>
+            </div>
+
+            <div style={{ display: "flex" }}>
+              <span>
+                <button class="shareBtn">
+                  <AiOutlineLink /> Share
+                </button>
+              </span>
+              <span class="lp">LP</span>
+              <span class="lp1">LP</span>
+              <span class="lp2">LP</span>
+            </div>
+          </div>
         </div>
+)
+          })}
+        
       </div>
     </div>
-  )
+  );
 }
--- src/components/UpperHeader.js
+++ src/components/UpperHeader.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./UpperHeader.css";
 import { GiSmartphone } from 'react-icons/gi';
+import { BiMenuAltLeft } from 'react-icons/bi';
+import { BiLaptop } from 'react-icons/bi';
+
+
 
 export default function UpperHeader() {
   return (
-    <div class='assess'>
+   <div>
+     <div class='assess'>
       <div class="box178">
         <div class="firstHead">Assessment</div>
         <div class="secondHeading">My Assessments</div>
@@ -13,5 +18,18 @@ export default function UpperHeader() {
         <div class="phoneIcon"><GiSmartphone /></div>
       </div>
     </div>
+    <div class='assess1'>
+      <div class="box178">
+        <div>
+            <BiMenuAltLeft/>
+        </div>
+        <div class="firstHead">Assessment</div>
+       
+      </div>
+      <div>
+        <div class="phoneIcon"><BiLaptop /></div>
+      </div>
+    </div>
+   </div>
   )
 }
