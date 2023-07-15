53a2f1c70eee756669bb5fa24ecc53b9b859bb94
puneetnexgen <puneet.pokhriyal@ngsmart.in>
2023-07-15 14:51:29 +0530

    assesss

--- src/components/MyAssess.js
+++ src/components/MyAssess.js
@@ -1,7 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import { AiOutlinePlus } from "react-icons/ai";
+import  "./MyAssess.css"
 export default function MyAssess() {
   return (
-    <div>MyAssess</div>
+    <div>
+      {/* heading  */}
+      <div> <h2>My Assessment</h2></div>
+
+      {/* 3 cards structure*/}
+      <div class="cardsMainBox">
+        {/* 1st card */}
+        <div class="newAssissmentBox">
+          <div><AiOutlinePlus></AiOutlinePlus></div>
+          <span>New Assessment</span>
+          <p>From here you can add questions of multiple types like MCQs, subjective (text or paragraph)!</p>
+        </div>
+        {/* 2nd card */}
+        <div  class="cards2Box">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
   )
 }
