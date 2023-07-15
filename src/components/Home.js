3df99a43fbf96d57d19f4c4dc4be070192a5dad8
Vishakharajput00 <rajputvr00feb@gmail.com>
2023-07-15 10:15:55 +0530

    sidebar

---
+++ src/components/Home.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { BiSolidDashboard } from "react-icons/bi";
+import { MdAssessment } from "react-icons/md";
+import { MdOutlineLibraryBooks } from "react-icons/md";
+import { GrStatusUnknown } from "react-icons/gr";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div>
+      <div class="row">
+        <div class="column">
+          <div class="box">
+            <BiSolidDashboard style={{ height: "20px", width: "20px" }} />
+            <h4 class="heading">Dashboard</h4>
+          </div>
+          <div class="box1">
+            <MdAssessment style={{ height: "20px", width: "20px" }} />
+            <h4 class="heading">Assessment</h4>
+          </div>
+          <div class="box">
+            <MdOutlineLibraryBooks style={{ height: "20px", width: "20px" }} />
+            <h4 class="heading">My Library</h4>
+          </div>
+          <hr class="hr" />
+
+          <div class="box">
+            <div>
+              {" "}
+              <button class="btn">Admin</button>
+            </div>
+            <GrStatusUnknown
+              style={{ height: "20px", width: "20px", paddingTop: "15px" }}
+            />
+            <h4 class="heading">Round Status</h4>
+          </div>
+        </div>
+
+        <div>
+          <div class="assess">
+            <div class='upperAssess'>
+<span class="firstHead">Assessment</span>
+<span>My Assessments</span>
+            </div>
+            <div>hello</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   
+  );
+}
